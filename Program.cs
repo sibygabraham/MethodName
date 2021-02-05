fc97d4f620298287950d8f4e9ae5b0915a180b5b
hello <hello@LAPTOP-NA2F1EVT>
2021-02-05 15:18:27 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MethodName
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            Writeyourname();
+        }
+        
+            public static void Writeyourname()
+        {
+            Console.Write("please Write your name ");
+            try
+            {
+                string name = Console.ReadLine();
+
+                string text = "hello" + name;
+                
+                Console.WriteLine ("Hello " + name);
+
+            }
+            
+
+            catch
+
+            {
+                Console.WriteLine(" Please input valuable data");
+            }
+          
+  
+        }
+           
+
+
+
+    }
+
+    }
+
